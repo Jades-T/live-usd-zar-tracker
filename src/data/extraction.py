b5e9a034b64e6aea362bcea4e0ddd80a3061b5be
Jady-Ray Tamboer <jatamscc-g-025@student.wethinkcode.co.za>
2026-09-15 05:43:41 +0200

    feat: added the class currency extractor to extraction.py cause I dislike seeing red in my tests: WIP

---
+++ src/data/extraction.py
@@ -0,0 +1,12 @@
+"""
+Data Extraction Module
+This module handles fetching USD/ZAR exchange rates from external APIs.
+It's the first step in the data engineering pipeline: Extract.
+"""
+
+class CurrencyExtractor:
+    """
+    A class to extract USD/ZAR exchange rate data from APIs.
+    This is the 'Extract' part of ETL (Extract, Transform, Load).
+    """
+    pass
