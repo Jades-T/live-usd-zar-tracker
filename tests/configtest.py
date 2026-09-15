2b1fa20789c1bc0531abff0fdfe22ac71193f7b6
Jady-Ray Tamboer <jatamscc-g-025@student.wethinkcode.co.za>
2026-09-15 05:35:31 +0200

    test: added the src directory to the python path, in order for the config file to be read and setup the test environment. done

--- tests/configtest.py
+++ tests/configtest.py
@@ -2,3 +2,10 @@
 Pytest configuration file
 This file sets up the test environment and fixtures.
 """
+import sys
+import os
+
+# add the src dir to the Python path:
+src_path = os.path.join(os.path.dirname(__file__), '..', 'src')
+if src_path not in sys.path:
+    sys.path.insert(0, src_path)
