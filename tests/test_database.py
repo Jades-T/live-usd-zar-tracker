2afd58b692dbb95f14fa4ac9b0b91db54da5bd6f
Jady-Ray Tamboer <jatamscc-g-025@student.wethinkcode.co.za>
2026-09-21 13:29:20 +0200

    test(database): added function to create a temporary database, that will be used for testing instead of using the actual database. Made use of pytest.fixture

--- tests/test_database.py
+++ tests/test_database.py
@@ -2,3 +2,31 @@
 Tests for the Database Storage Module
 These tests verify that the database operations work correctly.
 """
+
+import pytest
+import tempfile
+import os
+from src.database.storage import CurrencyDatabase
+
+
+
+class TestCurrencyDatabase:
+    """
+    Test class for the CurrencyDatabase.
+    """
+
+    @pytest.fixture
+    def temporary_db(self):
+        """
+        Creates a temporary database for testing
+        """
+
+        # Create a file that will be used for the database in the test.
+        with tempfile.NamedTemporaryFile(delete=False, suffix='.db') as temp_file:
+            db_path = temp_file.name
+
+        # This creates a database instance:
+        db = CurrencyDatabase(db_path)
+
+        # Clean up process: closes the connection and deletes the temporary file.
+        
