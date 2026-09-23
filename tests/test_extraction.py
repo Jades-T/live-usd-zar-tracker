791e290ef793d7c298fc18ab0c882d84b9b9287e
Jady-Ray Tamboer <jatamscc-g-025@student.wethinkcode.co.za>
2026-09-23 20:37:33 +0200

    test(extraction): Added the skeleton for all edge cases that will need to be implemented for the extraction test. WIP

--- tests/test_extraction.py
+++ tests/test_extraction.py
@@ -4,9 +4,29 @@ These tests verify that the currency extraction functionality works correctly.
 """
 
 import pytest
-from src.data.extraction import CurrencyExtractor
+from data.extract_data import CurrencyExtractor
 
 class TestCurrencyExtractor:
-    """
-    Test class for CurrencyExtractor functionality
-    """
+    """ Test class for CurrencyExtractor functionality."""
+
+    def test_extraction_initialization(self):
+        pass
+
+    def test_get_current_rate_structure(self):
+        pass
+
+    def test_get_current_rate_valid_values(self):
+        pass
+
+    def test_get_historical_data_structure(self):
+        pass
+
+    def test_get_historical_data_count(self):
+        pass
+
+    def test_get_historical_data_with_zero_days(self):
+        pass
+
+
+if __name__ == '__main__':
+    pytest.main([__file__], "-v")
